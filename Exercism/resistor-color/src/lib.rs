0c5a698e65405b8fc843f167db5586e0ccf00b1d
Daniel Kamenetsky <daniel.kamenetsky@gmail.com>
2022-07-13 17:52:22 -0400

    resistor-color exercism complete

--- Exercism/resistor-color/src/lib.rs
+++ Exercism/resistor-color/src/lib.rs
@@ -1,28 +1,56 @@
-#[derive(Debug, PartialEq)]
+// The goal of this exercise is to create a way:
+// - to look up the numerical value associated with a particular color band
+// - to convert the numerical value into a string representing color
+// - to list the different band colors
+use enum_iterator::IntoEnumIterator;
+use int_enum::IntEnum;
+#[repr(usize)]
+#[derive(Debug, PartialEq, Copy, Clone, IntEnum, IntoEnumIterator, Eq, Ord, PartialOrd)]
 pub enum ResistorColor {
-    Black,
-    Blue,
-    Brown,
-    Green,
-    Grey,
-    Orange,
-    Red,
-    Violet,
-    White,
-    Yellow,
+    Black = 0,
+    Blue = 6,
+    Brown = 1,
+    Green = 5,
+    Grey = 8,
+    Orange = 3,
+    Red = 2,
+    Violet = 7,
+    White = 9,
+    Yellow = 4,
 }
 
 pub fn color_to_value(_color: ResistorColor) -> usize {
-    unimplemented!("convert a color into a numerical representation")
+    // match _color {
+    //     ResistorColor::Black => 0,
+    //     ResistorColor:: Brown => 1,
+    //     ResistorColor:: Red => 2,
+    //     ResistorColor:: Orange => 3,
+    //     ResistorColor:: Yellow => 4,
+    //     ResistorColor:: Green => 5,
+    //     ResistorColor:: Blue => 6,
+    //     ResistorColor:: Violet => 7,
+    //     ResistorColor:: Grey => 8,
+    //     ResistorColor:: White => 9,
+    // unimplemented!("convert a color into a numerical representation")
+    _color.int_value()
 }
+
 
 pub fn value_to_color_string(value: usize) -> String {
-    unimplemented!(
-        "convert the value {} into a string representation of color",
-        value
-    )
+    match ResistorColor::from_int(value) {
+        Ok(color) => format!("{:?}", color),
+        _ => String::from("value out of range")
+    }
 }
 
+
 pub fn colors() -> Vec<ResistorColor> {
-    unimplemented!("return a list of all the colors ordered by resistance")
+    println!("submit test");
+    let mut collected = ResistorColor::into_enum_iter().collect::<Vec<ResistorColor>>();
+    collected.sort();
+    collected
+    
+
 }
+
+//submit test
